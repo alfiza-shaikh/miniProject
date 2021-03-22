887804dd59fc07d49dcb56bb3d8d1486841811b7
maaheynoor <maaheynoor.s@somaiya.edu>
2021-03-22 19:39:24 +0530

    Login Register separate pages

--- app/app.py
+++ app/app.py
@@ -20,6 +20,46 @@ bcrypt = Bcrypt(app)
 def index():
     return render_template('index.html')
 
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if "login-submit" in request.form:
+        form = Login(request.form)
+        print("Login form submitted")
+        print(form.errors)
+        if request.method == 'POST':
+            email=request.form['email']
+            password=bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
+            # bcrypt.check_password_hash(pw_hash, 'hunter2') 
+            print(email,password)
+        if form.validate():
+            database.authdb.login(email,password)
+            print('Logged in ' + email)
+        else:
+            flash('All the form fields are required. ')
+        
+        return render_template('login.html', loginform=form)
+    return render_template('login.html', loginform=None)
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if "register-submit" in request.form:
+        form = Register(request.form)
+        print("Register form submitted")
+        print(form.errors)
+        if request.method == 'POST':
+            name=request.form['name']
+            email=request.form['email']
+            password=bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
+            confirm_password=bcrypt.generate_password_hash(request.form['confirm-password']).decode('utf-8')
+            # bcrypt.check_password_hash(pw_hash, 'hunter2') 
+            
+            # if form.validate() and password==confirm_password:
+            database.authdb.register(name,email,password)
+        
+        return render_template('register.html',registerform=form)
+    return render_template('register.html',registerform=None)
+
+"""
 @app.route("/authenticate", methods=['GET', 'POST'])
 def authenticate():
     if "login-submit" in request.form:
@@ -49,14 +89,13 @@ def authenticate():
             confirm_password=bcrypt.generate_password_hash(request.form['confirm-password']).decode('utf-8')
             # bcrypt.check_password_hash(pw_hash, 'hunter2') 
             
-        # if form.validate() and password==confirm_password:
-        #     print(name,email,password)
+            # if form.validate() and password==confirm_password:
             database.authdb.register(name,email,password)
         
     
         return render_template('auth.html',loginform=None, registerform=form)
     return render_template('auth.html',loginform=None, registerform=None)
-
+"""
 class Login(Form):
     email = TextField('Email:', validators=[validators.required(), validators.Length(min=6, max=35)])
     password = TextField('Password:', validators=[validators.required(), validators.Length(min=3, max=35)])
--- app/database/authdb.py
+++ app/database/authdb.py
@@ -1,5 +1,25 @@
-from database.conn import connection
-
+# from database.conn import connection,cursor
+import psycopg2
 
 def register(name,email,password):
+    
     print("registered")
+    try:
+        connection = psycopg2.connect(user="postgres",
+                                        password="postgres",
+                                        host="127.0.0.1",
+                                        database="ALPR")
+
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO users (uname,email,pass) values('"+name+"','"+email+"','"+password+"')")
+        connection.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        connection.rollback()
+        connection.rollback()
+        print(error)
+    finally:
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+    
